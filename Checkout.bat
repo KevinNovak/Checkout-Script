8cfaaaae505e6ff27b5776c5b2cad89cbefadca5
Kevin Novak <kevinmnovak@yahoo.com>
2015-09-09 21:01:00 -0400

    Removed "start" from "wuapp.exe"

--- Checkout.bat
+++ Checkout.bat
@@ -92,7 +92,7 @@ if "%operatingSystem%"=="xp" (
 if "%operatingSystem%"=="ten" (
     start ms-settings:windowsupdate
 ) else ( 
-    start wuapp.exe
+    wuapp.exe
 )
 ping 1.1.1.1 -n 1 -w 600 > nul
 echo.
